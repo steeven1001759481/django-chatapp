a3873c5f791c149432e87b888cc5401783c3cc89
Steeven Pereira <pereirasteeven@gmail.com>
2024-12-04 12:51:45 +0530

    Add css and template for Chat Room

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.indexView)
+    path('', views.indexView, name='index'),
+    path('<str:room>/', views.roomView, name='room'),
+    path('checkview', views.checkView, name='check')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
 
 # Create your views here.
 def indexView(request):
     return render(request, 'index.html')
+
+def checkView(request):
+    roomName = request.POST['room_name']
+    userName = request.POST['user_name']
+    if Room.objects.filter(name=roomName).exists():
+        return redirect('/'+roomName+'/?user_name='+userName)
+    else:
+        newRoom = Room.objects.create(name=roomName)
+        newRoom.save()
+        return redirect('/'+roomName+'/?user_name='+userName)
+
+def roomView(request,room):
+    userName = ''
+    if request.GET:
+        userName = request.GET['user_name']
+        print(userName)
+    context = {
+        'room': room,
+        'user': userName
+    }
+    return render(request,'room.html', context)
